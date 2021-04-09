65e2a2a691950654d10d476861a6c25eabea97fb
platinumjs0203 <platinumjs0203@gmail.com>
2021-04-09 21:42:47 +0900

    get coordinate, weather condition

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
 import Loading from "./Loading";
 import Weather from "./Weather";
+import axios from 'axios';
+
+const API_KEY = "c862c3099b0324626bd7075d12cb7d47"
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude)
+    } catch (error) {
+      Alert.alert("위치동의좀 해줘요ㅠㅠ", "제발ㅠㅠ")
+    }
+  }
+  getWeather = async(latitude, longitude) => {
+    const {data: {main : {temp}, weather, name}} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log(temp)
+    console.log(weather[0].main)
+    console.log(name)
+    this.setState({
+      isLoading:false,
+      condition: weather[0].main,
+      temp,
+      name
+    })
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
   render(){
     const {isLoading} = this.state;
     return isLoading ? (<Loading />) : 
